a67d16f36e542acd69870917d5e168e5eafe6481
AungPyaePhyo10 <aungpyae.23@ichat.sp.edu.sg>
2024-12-05 19:45:17 +0800

    finished up readme.md + fixed admin bugs

--- src/public/admin/bookRequest.js
+++ src/public/admin/bookRequest.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+    fetch('admin_navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-container').innerHTML = data;
+
+        const logoutButton = document.getElementById('logout-button');
+        if (logoutButton) {
+            logoutButton.addEventListener('click', logout);
+        }
+    })
+
+    fetch('../footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer-container').innerHTML = data;
+    })
+
     fetchAllBookRequests(); // Load all book requests on page load
 
     // Fetch all book requests
@@ -105,7 +122,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 const author = e.target.getAttribute("data-author");
         
                 // Redirect to addbook.html with query parameters
-                window.location.href = `../addbook.html?bookName=${bookName}&author=${author}`;
+                window.location.href = `addbook.html?bookName=${bookName}&author=${author}`;
             });
         });
         
