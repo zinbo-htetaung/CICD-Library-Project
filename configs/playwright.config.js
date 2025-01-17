d12aad08c4d358c6cbf5de92ff5651de73ef9404
zinbohtetaung <zinbo.23@ichat.sp.edu.sg>
2025-01-17 09:22:29 +0800

    testing

--- configs/playwright.config.js
+++ configs/playwright.config.js
@@ -62,7 +62,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: `http://127.0.0.1:${process.env.PORT || 3000}`,
+    url: 'http://127.0.0.1:3001',
     reuseExistingServer: !process.env.CI,
   },
 
